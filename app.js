64b47688fa50f02bed4556b41331af1f41067760
VernonTamba <tambzfantastico@gmail.com>
2021-01-19 13:14:19 +0800

    Added JS Codes
    Display Hello World in the console

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+// Display Hello World in the h1 (index.html)
 const title = document.querySelector(".title");
 title.textContent = "Hello World";
+
+// Display Hello World in the console
+console.log("Hello World");
